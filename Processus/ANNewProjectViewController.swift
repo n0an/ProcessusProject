0172ed66aeed3f0cae207e1b56b98746d5e682c1
n0an <nantog@gmail.com>
2016-05-14 17:28:59 +0300

    new project vc ui added

---
+++ Processus/ANNewProjectViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ANNewProjectViewController.swift
+//  Processus
+//
+//  Created by Anton Novoselov on 14/05/16.
+//  Copyright © 2016 Anton Novoselov. All rights reserved.
+//
+
+import UIKit
+
+class ANNewProjectViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var bottomConstraint: NSLayoutConstraint!
+    
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    
+
+}
---
+++ Processus/ANProjectInfoTextCell.swift
@@ -0,0 +1,24 @@
+//
+//  ANProjectInfoTextCell.swift
+//  Processus
+//
+//  Created by Anton Novoselov on 14/05/16.
+//  Copyright © 2016 Anton Novoselov. All rights reserved.
+//
+
+import UIKit
+
+class ANProjectInfoTextCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
