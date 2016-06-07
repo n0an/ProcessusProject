b2f41d266193a754686426a709a7c02062671fda
n0an <nantog@gmail.com>
2016-06-07 20:36:33 +0300

    added ANFinishedProjectsVC

---
+++ Processus/ANFinishedProjetsViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ANFinishedProjetsViewController.swift
+//  Processus
+//
+//  Created by Anton Novoselov on 07/06/16.
+//  Copyright © 2016 Anton Novoselov. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+
+class ANFinishedProjetsViewController: UIViewController {
+    
+    // MARK: - OUTLETS
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    // MARK: - ATTRIBUTES
+
+    private var fetchedResultsController: NSFetchedResultsController!
+
+    
+    // MARK: - viewDidLoad
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+        
+        
+        
+    }
+
+    
+
+}
